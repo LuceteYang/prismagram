24dbe2112f99cefe73dd7d0da54d99b76f16175e
sanghwan <jae6120@naver.com>
2019-07-24 18:11:42 +0800

    #3.8 toggleLike and addComment Resolver

--- src/api/Like/toggleLike/toggleLike.js
+++ src/api/Like/toggleLike/toggleLike.js
@@ -1,3 +1,4 @@
+
 import { isAuthenticated } from "../../../middlewares";
 import { prisma } from "../../../../generated/prisma-client";
 
@@ -7,35 +8,24 @@ export default {
       isAuthenticated(request);
       const { postId } = args;
       const { user } = request;
-      try {
-        const existingLike = await prisma.$exists.like({
-          AND: [
-            {
-              user: {
-                id: user.id
-              }
-            },
-            {
-              post: {
-                id: postId
-              }
-            }
-          ]
-        });
-        if (existingLike) {
-          // TO DO
-          await prisma.deleteLike({
+      const filterOptions = {
+        AND: [
+          {
             user: {
-                connect: {
-                  id: user.id
-                }
-              },
-              post: {
-                connect: {
-                  id: postId
-                }
-              }
-          })
+              id: user.id
+            }
+          },
+          {
+            post: {
+              id: postId
+            }
+          }
+        ]
+      };
+      try {
+        const existingLike = await prisma.$exists.like(filterOptions);
+        if (existingLike) {
+          await prisma.deleteManyLikes(filterOptions);
         } else {
           await prisma.createLike({
             user: {
